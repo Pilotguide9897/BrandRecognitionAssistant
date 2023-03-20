699927665844bad18dec76da85b63153a59b6442
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-03-20 14:11:15 -0300

    re-installed dependencies

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const autocomplete = require('inquirer-autocomplete-prompt');
-const fuzzy = require('fuzzy');
-const color = require('colors');
-const myColours = require('./Library/colours.js');
+// const autocomplete = require('inquirer-autocomplete-prompt');
+// const fuzzy = require('fuzzy');
+// const color = require('colors');
+// const myColours = require('./Library/colours.js');
 
-inquirer.registerPrompt('autocomplete', autocomplete);
+// inquirer.registerPrompt('autocomplete', autocomplete);
 
-const colourOptions = {
-    extract: (el) => Object.keys(el)
-};
-
+// const colourOptions = {
+//     extract: (el) => Object.keys(el)
+// };
+const myPrompt = async () => {
 const questions = [    
     {
         type: 'input',
@@ -25,60 +25,69 @@ const questions = [
                 return true;
             }
         }
-     },
-     {
-        type: 'autocomplete',
-        name: 'textColor',
-        message: 'Enter the color for the text. You may provide either a colour name keyword or a valid hexadecimal value:',
-        source: (answers,input) => {
-          const fuzzyResult = fuzzy.filter(input || '', myColours, options);
-          const results = fuzzyResult.map((el) => el.original);
-          return results;
-      },
-      validate: function(value) {
-        if (this.answers.shapeColorType === 'Color keyword') {
-          return (value.match(/^[a-zA-Z]+$/) ? true : 'Please enter a valid color keyword');
-        } else {
-          return (value.match(/^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$/) ? true : 'Please enter a valid hexadecimal color number');
-          }
-        }
-      },
-    {
-        type: 'list',
-        message: 'Please choose a shape for your logo.',
-        name: 'logoShape',
-        choices: ['Circle', 'Triangle', 'Square']
-    },
-    {
-        type: 'autocomplete',
-        name: 'shapeColor',
-        message: 'Enter the color for the shape. You may provide either a colour name keyword or a valid hexadecimal value:',
-        source: (answers,input) => {
-            const fuzzyResult = fuzzy.filter(input || '', myColours, options);
-            const results = fuzzyResult.map((el) => el.original);
-            return results;
-        },
-        validate: function(value) {
-          if (this.answers.shapeColorType === 'Color keyword') {
-            return (value.match(/^[a-zA-Z]+$/) ? true : 'Please enter a valid color keyword');
-          } else {
-            return (value.match(/^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$/) ? true : 'Please enter a valid hexadecimal color number');
-          }
-        }
-    }
-  ];
+     }
+    ]
+  //    {
+  //       type: 'autocomplete',
+  //       name: 'textColor',
+  //       message: 'Enter the color for the text. You may provide either a colour name keyword or a valid hexadecimal value:',
+  //       source: (answers,input) => {
+  //         const fuzzyResult = fuzzy.filter(input || '', myColours, options);
+  //         const results = fuzzyResult.map((el) => el.original);
+  //         return results;
+  //     },
+  //     validate: function(value) {
+  //       if (this.answers.shapeColorType === 'Color keyword') {
+  //         return (value.match(/^[a-zA-Z]+$/) ? true : 'Please enter a valid color keyword');
+  //       } else {
+  //         return (value.match(/^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$/) ? true : 'Please enter a valid hexadecimal color number');
+  //         }
+  //       }
+  //     },
+  //   {
+  //       type: 'list',
+  //       message: 'Please choose a shape for your logo.',
+  //       name: 'logoShape',
+  //       choices: ['Circle', 'Triangle', 'Square']
+  //   },
+  //   {
+  //       type: 'autocomplete',
+  //       name: 'shapeColor',
+  //       message: 'Enter the color for the shape. You may provide either a colour name keyword or a valid hexadecimal value:',
+  //       source: (answers,input) => {
+  //           const fuzzyResult = fuzzy.filter(input || '', myColours, options);
+  //           const results = fuzzyResult.map((el) => el.original);
+  //           return results;
+  //       },
+  //       validate: function(value) {
+  //         if (this.answers.shapeColorType === 'Color keyword') {
+  //           return (value.match(/^[a-zA-Z]+$/) ? true : 'Please enter a valid color keyword');
+  //         } else {
+  //           return (value.match(/^#([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3})$/) ? true : 'Please enter a valid hexadecimal color number');
+  //         }
+  //       }
+  //   }
+  // ];
+
+  return inquirer.prompt(questions);
+};
+
+  module.exports = questions;
+
+
   
   inquirer.prompt(questions)
     .then(answers => {
-      console.log(`The text color is: ${color[answers.textColor](answers.textColorType)}`);
+      //console.log(`The text color is: ${color[answers.textColor](answers.textColorType)}`);
+      console.log('success');
     })
     .catch(error => {
       console.log(error);
     });
 
-    const renderShape = {
+    // const renderShape = {
 
-    }
+    // }
 
     // fs.writeFile('logo.svg', ,(err) => { // Need to include the data to take in.
     //     if (err) throw new Error('An error occurred writing your file.');
