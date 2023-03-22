71be12816b44ce661b6eefe089c11efa06c63a16
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-03-22 09:19:17 -0300

    fixed error with failed shape rendering tests.

--- Library/Shapes/square.js
+++ Library/Shapes/square.js
@@ -2,7 +2,7 @@ const Shapes = require("./shapes");
 
 class Square extends Shapes {
       render() {
-        return `<rect x="60" y="60" width="180" height="180" fill="${this.color}" />`;
+        return `<rect x="60" y="60" width="180" height="180" fill="${this.colour}" />`;
       }
     }
 
